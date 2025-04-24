e98d46a6ac9369b35fd4d2246324fee54e0e716d
08abhinav <abhinavnegi101@gmail.com>
2025-04-24 21:27:45 +0530

    feat: implemented superTokens

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,58 @@
+import 'dotenv/config'
 import express from "express";
+import cors from 'cors'
+import supertokens from "supertokens-node";
+import Session from "supertokens-node/recipe/session";
+import EmailPassword from "supertokens-node/recipe/emailpassword";
+import { middleware } from "supertokens-node/framework/express";
+
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
-// Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// Test route
-app.get("/", (req:any, res:any) => {
-  res.json({ message: "Express + TypeScript Server is running" });
+supertokens.init({
+  framework: "express",
+  supertokens: {
+      // We use try.supertokens for demo purposes.
+      // At the end of the tutorial we will show you how to create
+      // your own SuperTokens core instance and then update your config.
+      connectionURI: "https://try.supertokens.io",
+      // apiKey: <YOUR_API_KEY>
+  },
+  appInfo: {
+    // learn more about this on https://supertokens.com/docs/session/appinfo
+    appName: "trading-backend",
+    apiDomain: "http://localhost:8000",
+    websiteDomain: "http://localhost:8000",
+    apiBasePath: "/api/auth",
+    websiteBasePath: "/api/auth",
+  },
+  recipeList: [
+    EmailPassword.init(), // initializes signin / sign up features
+    Session.init() // initializes session features
+    ]
 });
 
-// Start server
+
+app.use(
+	cors({
+		origin: "http://localhost:8000",
+		allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+		credentials: true,
+	}),
+);
+
+app.use(middleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", userRoutes); 
+
+app.get("/", (req, res) => {
+  res.json({ message: "Express + TypeScript server is running" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
